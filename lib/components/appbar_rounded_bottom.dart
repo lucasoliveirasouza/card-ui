20cd3a7e7b18ed03c421d9d4f7d71ae59eb04f98
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-23 23:42:00 -0300

    Criando AppBar

--- lib/components/appbar_rounded_bottom.dart
+++ lib/components/appbar_rounded_bottom.dart
@@ -1,3 +1,4 @@
+import 'package:cardui/util/constants.dart';
 import 'package:flutter/material.dart';
 
 class AppbarRoundedBottom extends StatelessWidget {
@@ -5,6 +6,45 @@ class AppbarRoundedBottom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 230,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(50),
+        ),
+        color: defaultColor,
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 80,
+            left: 0,
+            child: Container(
+              height: 100,
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 115,
+            left: 20,
+            child: Text(
+              "Your Books",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: defaultColor,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -13,6 +13,7 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: [
           AppbarRoundedBottom(),
