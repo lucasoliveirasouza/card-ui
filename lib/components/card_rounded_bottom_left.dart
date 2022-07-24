368a0e491d4b3f5e6f5d03f7ee04bd0cc6cc858f
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-24 00:42:22 -0300

    Criando componentes

--- lib/components/card_rounded_bottom_left.dart
+++ lib/components/card_rounded_bottom_left.dart
@@ -12,11 +12,19 @@ class CardRoundedBottomLeft extends StatelessWidget {
       padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
       child: Container(
         decoration: BoxDecoration(
-            color: defaultColor,
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(80),
+          color: defaultColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(80),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: defaultColor.withOpacity(0.3),
+              offset: new Offset(-10.0, 10.0),
+              blurRadius: 20.0,
+              spreadRadius: 4.0,
             ),
-            boxShadow: [new BoxShadow()]),
+          ],
+        ),
       ),
     );
   }
